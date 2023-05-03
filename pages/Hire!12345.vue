3367e837ecb97a33dbcceef592fac39fbea5cabd
Zohreh <51000344+PanteaMartia@users.noreply.github.com>
2023-05-03 11:45:56 -0400

    hire page changes
    hi

--- pages/Hire!12345.vue
+++ pages/Hire!12345.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-    name: 'Hire'
+    name: 'Hire me!'
 }
 </script>
 
--- pages/Join.vue
+++ pages/Join.vue
@@ -1,5 +1,7 @@
 <template>
-    <div></div>
+    <div>
+        <h1>{{ text }}</h1>
+    </div>
 </template>
 
 <script>
