f97afeaa71da5d2de724e465f7a609d572edd51c
vitaly-savin <105252622+vitaly-savin@users.noreply.github.com>
2023-05-08 23:04:18 -0400

    Create VitalyForm.vue

---
+++ pages/VitalyForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <OpenFormVitaly @click="showForm"></OpenFormVitaly>
+        <YourNewFormComponent @created="addItem" @cancel="hideForm" />
+        <ul>
+            <li v-for="item in items" :key="item.name">
+                {{item.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import OpenFormVitaly from '~/components/buttons/OpenFormVitaly.vue'
+export default {
+    name: 'VitalyForm',
+    components: {OpenFormVitaly},
+    data () {
+        return {
+            isFormShown: false,
+            items: [
+                {
+                    name: 'VitalyForm'
+                }
+            ]
+        }
+    },
+    methods: {
+        showAddForm() {
+            this.formShown = true
+        },
+        memberCreated(newMember) {
+            const newId = Math.ceil(Math.random() * 50000)
+            newMember.id = newId
+            this.members.push({
+                id: newId,
+                data: newMember
+            })
+            this.hideForm()
+        },
+        hideForm () {
+            this.formShown = false
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
