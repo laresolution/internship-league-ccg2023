dc9e431819da638711cfbf74650344a348b9e752
TianaSantatraR <tianasantatramusic@gmail.com>
2023-05-12 03:06:00 -0400

    creating member's form

---
+++ components/TRform.vue
@@ -0,0 +1,44 @@
+<template>
+     <div>
+    <v-text-field v-model="fullName"
+      label="Your first and last name"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <v-text-field v-model="profession" label="Your profession"></v-text-field>
+    <v-text-field v-model="likes" label="Your likes"></v-text-field>
+    <v-btn
+        class="mx-2"
+        fab
+        color="primary"
+        @click="clicked"
+    >
+        <v-icon dark>
+            mdi-plus
+        </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TRform',
+    data: () => ({
+        
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+      fullName:'',
+      profession:'',
+      likes:''
+    }),
+    methods: {
+        clicked() {
+            this.$emit('newMember', this.fullName,this.profession,this.likes);
+        }
+    }
+  }
+</script>
+
+<style scoped></style>
--- pages/TianaRabezandriny.vue
+++ pages/TianaRabezandriny.vue
@@ -1,15 +1,56 @@
 <template>
     <div>
+        <TRform v-on:newMember="affiche($event)">
 
+        </TRform>
+        <v-card class="mx-auto" max-width="344" outlined>
+            <v-list-item three-line>
+                <v-list-item-content>
+                    <div class="text-overline mb-4">
+                        OVERLINE
+                    </div>
+                    <v-list-item-title class="text-h5 mb-1">
+                        {{ salutation }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+                </v-list-item-content>
 
-        
+                <v-list-item-avatar tile size="80" color="blue"></v-list-item-avatar>
+            </v-list-item>
+
+            <!-- <v-card-actions>
+        <v-btn
+          outlined
+          rounded
+          text
+        >
+          Button
+        </v-btn>
+      </v-card-actions> -->
+        </v-card>
     </div>
 </template>
 
 <script>
+import TRform from '~/components/TRform.vue';
 export default {
-    name: 'TianaRabezandriny'
-}
+    name: 'TianaRabezandriny',
+    components:{TRform},
+    data() {
+        return { salutation: 'HELLO',
+        fullName:'',
+      profession:'',
+      likes:''
+     
+    },
+    methods: {
+        affiche:function(données){
+            this.fullName= données.fullName;
+
+        } 
+    }
+
+}}
 </script>
 
 <style scoped></style>
