d9a4e9b4f1ec4f85280d8e667d80c4ad7864bfe3
houzhihuil <philipwhole@gmail.com>
2023-05-16 19:07:56 -0400

    Update AddMember.vue

--- components/AddMember.vue
+++ components/AddMember.vue
@@ -3,12 +3,25 @@
         <v-container>
             <v-row>
                 <v-col>
-                    <v-text-field label="Name" v-model="newMember.name"></v-text-field>
-                    <v-text-field label="Image URL" v-model="newMember.imgSrc"></v-text-field>
-                    <v-text-field label="Title" v-model="newMember.title"></v-text-field>
-                    <v-textarea label="Quote" v-model="newMember.quote"></v-textarea>
-                    <v-textarea label="Strengths" v-model="newMember.strengths"></v-textarea>
-                    <v-textarea label="Likes" v-model="newMember.likes"></v-textarea>
+                    <v-text-field label="Name" v-model="newMember.name" :rules="nameRules" :maxlength="60" required></v-text-field>
+                    <v-text-field label="Email" v-model="newMember.email" :rules="emailRules" required></v-text-field>
+                    <v-text-field label="Student number" v-model="newMember.studentNumber" :maxlength="20" required></v-text-field>
+                    <v-text-field label="Program number" v-model="newMember.programNumber" :maxlength="20" required></v-text-field>
+                    <v-text-field label="Institution" v-model="newMember.institution" :maxlength="100"></v-text-field>
+                    <v-text-field label="Supervisor name" v-model="newMember.supervisorName" :maxlength="60"></v-text-field>
+                     Internship start 
+                    <v-row justify="space-around">
+                        <v-date-picker
+                        v-model="picker" :min="new Date().toISOString().split('T')[0]" required
+                        color="green lighten-1"
+                        ></v-date-picker>
+                        <v-date-picker
+                        v-model="picker2"
+                        color="green lighten-1"
+                        header-color="primary" :max="maxDate"
+                        ></v-date-picker>
+                    </v-row>
+                    
                     <SaveButton @click="saveMember"></SaveButton>
                     <CancelButton @click="clear"></CancelButton>
                 </v-col>
@@ -23,12 +36,13 @@ import SaveButton from './buttons/SaveButton.vue'
 
 const generateNewMember = () => {
     return {
-        imgSrc: '',
         name: '',
-        title: '',
-        quote: '',
-        strengths: '',
-        likes: ''
+        email: '',
+        studentNumber: '',
+        programNumber: '',
+        institution: '',
+        supervisorName: '',
+       
     }
 }
 export default {
@@ -36,7 +50,20 @@ export default {
     components: { CancelButton, SaveButton },
     data() {
         return {
-            newMember: generateNewMember()
+            picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+            picker2: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+            maxDate: this.getMaxDate(),
+            newMember: generateNewMember(),
+
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+      
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
         };
     },
     methods: {
@@ -47,7 +74,12 @@ export default {
         clear() {
             this.$emit("cancel")
             this.newMember = generateNewMember();
-        }
+        },
+        getMaxDate() {
+            const date = new Date();
+            date.setFullYear(date.getFullYear() + 2);
+            return date.toISOString().substr(0, 10);
+        },
     }
 }
 </script>
