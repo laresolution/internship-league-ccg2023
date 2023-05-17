67a7aa83417b19ff6cf2701d6f6c278a5f41e840
houzhihuil <philipwhole@gmail.com>
2023-05-16 22:39:44 -0400

    Create newlayout2.vue

---
+++ layouts/newlayout2.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <div>Bonjour {{ currentMember.name }}!</div>
+        
+        <Nuxt />
+    </v-app>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters('members', [
+            'currentMember'
+        ])
+    }
+}
+</script>
+<style scoped type="scss">
+
+</style>
