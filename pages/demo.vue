63592f5f5635330dc39e14ba9a2141b4fdf7a9af
houzhihuil <philipwhole@gmail.com>
2023-05-16 22:05:30 -0400

    Create demo.vue

---
+++ pages/demo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <AddDialogVue></AddDialogVue> 
+    </div>
+</template>
+
+<script>
+import AddDialogVue from '~/components/AddDialog.vue';
+ 
+
+export default {
+    name: "demo",
+    components: {  AddDialogVue },
+    data() {
+        return {}
+    },
+   
+}
+</script>
+
+<style scoped></style>
