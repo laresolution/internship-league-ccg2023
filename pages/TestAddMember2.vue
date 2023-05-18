34a04913d3f313c6bd37c8720d95304913da83c5
houzhihuil <philipwhole@gmail.com>
2023-05-18 11:13:36 -0400

    Create TestAddMember2.vue

---
+++ pages/TestAddMember2.vue
@@ -0,0 +1,34 @@
+<template>
+    <AddMember></AddMember>
+</template>
+
+<script>
+import AddMember from '../components/AddMember.vue'
+ 
+ 
+export default {
+    name: "TestAddMember2",
+    components: { AddMember },
+    data() {
+        return {  
+            // members: [], // Array to store new members
+ 
+        };
+    },
+    methods: {
+/*         saveMember() {
+            // Push the new member object to the array
+        // this.members.push(this.newMember); 
+        console.log(this.newMember);
+        console.log('*** Save ***');  
+        console.log(this.members); 
+        },
+        clear() {
+            console.log(this.newMember); 
+            console.log('*** Cancel ***'); 
+            console.log(this.members);
+        }   */
+    } 
+}
+
+</script>
