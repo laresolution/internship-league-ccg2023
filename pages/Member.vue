5a088084b43325eb1ef37252c1ed1b707431e8e9
Zohreh <51000344+PanteaMartia@users.noreply.github.com>
2023-05-18 14:57:37 -0400

    MemberStyle

---
+++ pages/Member.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+        <AddButton @click="showAddForm"></AddButton>
+            <v-card v-if="formShown">
+                <v-card-title>Add new member</v-card-title>
+                <v-card-text>                    
+                    <AddMember @created="memberCreated" @cancel="hideForm"></AddMember>
+                </v-card-text>
+            </v-card>
+            <MemberStyle></MemberStyle>
+    </div>
+    </template>
+
+    <script>
+import AddButton from '~/components/buttons/AddButton.vue';
+import AddMember from '~/components/AddMember.vue';
+import MemberStyle from '~/components/MemberStyle.vue';
+    export default{
+    name: "Member",
+    components: { AddButton, AddMember, MemberStyle }
+}
+</script>
