a3c43d1af0913bf841f68c033061b353d0f6a1c9
houzhihuil <philipwhole@gmail.com>
2023-05-17 22:24:15 -0400

    Update AddMember.vue

--- components/AddMember.vue
+++ components/AddMember.vue
@@ -49,8 +49,7 @@ const generateNewMember = () => {
         institution: '',
         supervisorName: '',
         startDate:'',
-        endDate:''
-        
+        endDate:'' 
          
     }
 }
