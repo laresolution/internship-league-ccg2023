28335d6df98c2814e2b3957a781c28e860cec679
houzhihuil <philipwhole@gmail.com>
2023-05-18 13:18:39 -0400

    Create TestAddMember3.vue

---
+++ pages/TestAddMember3.vue
@@ -0,0 +1,22 @@
+<template>
+    <AddDialog></AddDialog>
+</template>
+
+<script>
+import AddDialog from '../components/AddDialog.vue'
+ 
+ 
+export default {
+    name: "TestAddMember3",
+    components: { AddDialog },
+    data() {
+        return {  
+            // members: [], // Array to store new members
+ 
+        };
+    },
+    methods: { 
+    } 
+}
+
+</script>
