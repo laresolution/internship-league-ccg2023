e978ede2b17ffcf467379d5074733c78144bf840
houzhihuil <philipwhole@gmail.com>
2023-05-17 21:46:24 -0400

    Update MemberForm.vue

--- pages/MemberForm.vue
+++ pages/MemberForm.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <router-link to="demo">demo</router-link>
-        {{ currentMember.id }} 
-        <AddDialog></AddDialog>
+        {{ currentMember.id }}  
+        <AddButton @click="addMember"></AddButton>
+    
+        <AddDialogVue ></AddDialogVue>
+        
         <ul>
             <li v-for="member in members" :key="member.id">
-                {{member.name}}
+                {{member.name}}   
+                {{member.email}}
+                {{member.phone}}   
+                {{member.studentNumber}}
+                {{member.programNumber}}   
+                {{member.institution}}
+                {{member.supervisorName}}   
+                {{member.startDate}}
+                {{member.endDate}} 
             </li>
         </ul>
     </div>
@@ -13,11 +24,12 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import AddDialog from '~/components/AddDialog.vue'; 
-
+import AddButton from '~/components/buttons/AddButton.vue';
+ 
+import AddDialogVue from '~/components/AddDialog.vue';
 export default {
     name: "MemberForm",
-    components: {  AddDialog },
+    components: { AddButton,  AddDialogVue},
     layout: "newlayout2",
     data() {
         return {
@@ -42,12 +54,19 @@ export default {
         ...mapActions('members', [
             'addMember'
         ]),
-        addRandomMember() {
+        addMember() {
             const id = Math.ceil(Math.random() * 100000)
             this.addMember({
                 id,
-                name: `New Member (${id})`,
-                favoriteColor: 'Green'
+                name: `New Member (${id})`, 
+                email: 'p@gmail.com',
+                phone: '5142223344',
+                studentNumber: '123456789',
+                programNumber: '123456789',
+                institution: '123456789',
+                supervisorName: '123456789',
+                startDate:'20230518',
+                endDate:'20230528'  
             })
         }
     }
