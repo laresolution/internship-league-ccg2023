f9ceb2e81bbd80f052a3c9d02ad9c14852841b2c
houzhihuil <philipwhole@gmail.com>
2023-05-18 15:31:38 -0400

    Update MemberForm.vue

--- pages/MemberForm.vue
+++ pages/MemberForm.vue
@@ -1,44 +1,11 @@
 <template>
     <div>
-        <router-link to="demo">demo</router-link> 
-        <AddButton @click="addMember"></AddButton> 
-        <AddDialogVue ></AddDialogVue>
-<!--             <template>
-                <v-form>
-                    <v-container>
-                        <v-row>
-                            <v-col>
-                        <v-text-field label="Name" v-model="newMember.name" :rules="nameRules" :maxlength="60" required></v-text-field>
-                        <v-text-field label="Email" v-model="newMember.email" :rules="emailRules" required></v-text-field>
-                        <v-text-field label="Phone" v-model="newMember.phone" :rules="phoneRules" required></v-text-field>
-                        <v-text-field label="Student number" v-model="newMember.studentNumber" :maxlength="20" required></v-text-field>
-                        <v-text-field label="Program number" v-model="newMember.programNumber" :maxlength="20" required></v-text-field>
-                        <v-text-field label="Institution" v-model="newMember.institution" :maxlength="100"></v-text-field>
-                        <v-text-field label="Supervisor name" v-model="newMember.supervisorName" :maxlength="60"></v-text-field>
-                        
-                        <v-row justify="space-around"> 
-                            <div>Internship start
-                            <v-date-picker
-                            v-model="pickerStart" :min="new Date().toISOString().split('T')[0]" required
-                            color="green lighten-1"
-                            ></v-date-picker></div>
-
-                            <div>Internship end 
-                            <v-date-picker
-                            v-model="pickerEnd"
-                            color="green lighten-1"
-                            header-color="primary" :max="maxDate"
-                            ></v-date-picker></div>
-                            <div>
-                            <SaveButton @click="saveMember"></SaveButton>
-                            <CancelButton @click="clear">Cancel</CancelButton></div>
-                        </v-row> 
-                        
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-form>
-            </template> -->
+        <router-link to="MemberForm">Member Form</router-link> 
+        {{ currentMember.id }}  
+        <AddButton @click="addRandomMember"></AddButton>
+    
+        <AddDialog></AddDialog>
+        
         <ul>
             <li v-for="member in members" :key="member.id">
                 {{member.name}}   
@@ -59,10 +26,10 @@
 import { mapGetters, mapActions } from 'vuex'
 import AddButton from '~/components/buttons/AddButton.vue';
  
-import AddDialogVue from '~/components/AddDialog.vue';
+import AddDialog from '~/components/AddDialog.vue';
 export default {
     name: "MemberForm",
-    components: { AddButton,  AddDialogVue},
+    components: { AddButton,  AddDialog},
     layout: "newlayout2",
     data() {
         return {
@@ -87,19 +54,19 @@ export default {
         ...mapActions('members', [
             'addMember'
         ]),
-        addMember() {
+        addRandomMember() {
             const id = Math.ceil(Math.random() * 100000)
             this.addMember({
                 id,
-                name: '', 
-                email: '',
-                phone: '',
-                studentNumber: '',
-                programNumber: '',
-                institution: '',
-                supervisorName: '',
-                startDate:'',
-                endDate:'', 
+                name: `New Member (${id})`, 
+                email: 'p@gmail.com',
+                phone: '5142223344',
+                studentNumber: '123456789',
+                programNumber: '123456789',
+                institution: '123456789',
+                supervisorName: '123456789',
+                startDate:'20230518',
+                endDate:'20230528'  
             })
         }
     }
@@ -107,4 +74,4 @@ export default {
 </script>
 
 <style scoped>
-</style>
+</style> 
