e994c797b2fcde61506d9087155b61f65d869f24
houzhihuil <philipwhole@gmail.com>
2023-05-17 22:24:49 -0400

    Update MemberForm.vue

--- pages/MemberForm.vue
+++ pages/MemberForm.vue
@@ -1,11 +1,44 @@
 <template>
     <div>
-        <router-link to="demo">demo</router-link>
-        {{ currentMember.id }}  
-        <AddButton @click="addMember"></AddButton>
-    
+        <router-link to="demo">demo</router-link> 
+        <AddButton @click="addMember"></AddButton> 
         <AddDialogVue ></AddDialogVue>
-        
+<!--             <template>
+                <v-form>
+                    <v-container>
+                        <v-row>
+                            <v-col>
+                        <v-text-field label="Name" v-model="newMember.name" :rules="nameRules" :maxlength="60" required></v-text-field>
+                        <v-text-field label="Email" v-model="newMember.email" :rules="emailRules" required></v-text-field>
+                        <v-text-field label="Phone" v-model="newMember.phone" :rules="phoneRules" required></v-text-field>
+                        <v-text-field label="Student number" v-model="newMember.studentNumber" :maxlength="20" required></v-text-field>
+                        <v-text-field label="Program number" v-model="newMember.programNumber" :maxlength="20" required></v-text-field>
+                        <v-text-field label="Institution" v-model="newMember.institution" :maxlength="100"></v-text-field>
+                        <v-text-field label="Supervisor name" v-model="newMember.supervisorName" :maxlength="60"></v-text-field>
+                        
+                        <v-row justify="space-around"> 
+                            <div>Internship start
+                            <v-date-picker
+                            v-model="pickerStart" :min="new Date().toISOString().split('T')[0]" required
+                            color="green lighten-1"
+                            ></v-date-picker></div>
+
+                            <div>Internship end 
+                            <v-date-picker
+                            v-model="pickerEnd"
+                            color="green lighten-1"
+                            header-color="primary" :max="maxDate"
+                            ></v-date-picker></div>
+                            <div>
+                            <SaveButton @click="saveMember"></SaveButton>
+                            <CancelButton @click="clear">Cancel</CancelButton></div>
+                        </v-row> 
+                        
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-form>
+            </template> -->
         <ul>
             <li v-for="member in members" :key="member.id">
                 {{member.name}}   
@@ -58,15 +91,15 @@ export default {
             const id = Math.ceil(Math.random() * 100000)
             this.addMember({
                 id,
-                name: `New Member (${id})`, 
-                email: 'p@gmail.com',
-                phone: '5142223344',
-                studentNumber: '123456789',
-                programNumber: '123456789',
-                institution: '123456789',
-                supervisorName: '123456789',
-                startDate:'20230518',
-                endDate:'20230528'  
+                name: '', 
+                email: '',
+                phone: '',
+                studentNumber: '',
+                programNumber: '',
+                institution: '',
+                supervisorName: '',
+                startDate:'',
+                endDate:'', 
             })
         }
     }
