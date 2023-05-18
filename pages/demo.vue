57b78a7a1a290c031437a262291daf01d7f45815
houzhihuil <philipwhole@gmail.com>
2023-05-17 21:46:21 -0400

    Update demo.vue

--- pages/demo.vue
+++ pages/demo.vue
@@ -1,59 +1,77 @@
 <template>
-    <div>
-        <router-link to="MemberForm">Member Form</router-link>
-        {{ currentMember.id }}
-        <!-- <AddButton @click="addRandomMember"></AddButton> -->
-        <AddDialog></AddDialog>
-        <ul>
-            <li v-for="member in members" :key="member.id">
-                {{member.name}}
-            </li>
-        </ul>
+            <div>
+                <router-link to="MemberForm">Member Form</router-link> 
+                {{ currentMember.id }}  
+                <AddButton @click="addRandomMember"></AddButton>
+            
+                <AddDialogVue ></AddDialogVue>
+                
+                <ul>
+                    <li v-for="member in members" :key="member.id">
+                        {{member.name}}   
+                        {{member.email}}
+                        {{member.phone}}   
+                        {{member.studentNumber}}
+                        {{member.programNumber}}   
+                        {{member.institution}}
+                        {{member.supervisorName}}   
+                        {{member.startDate}}
+                        {{member.endDate}} 
+                    </li>
+                </ul>
+            </div>
+        </template>
+        
+        <script>
+        import { mapGetters, mapActions } from 'vuex'
+        import AddButton from '~/components/buttons/AddButton.vue';
          
-    </div>
-</template>
-
-<script>
-import { mapGetters, mapActions } from 'vuex'; 
-import AddDialog from '~/components/AddDialog.vue';
-
-export default {
-    name: "demo",
-    components: { AddDialog },
-    data() {
-        return {
-            isFormShown: false,
-            items: [
-                {
-                    name: "Empty Member"
+        import AddDialogVue from '~/components/AddDialog.vue';
+        export default {
+            name: "MemberForm",
+            components: { AddButton,  AddDialogVue},
+            layout: "newlayout2",
+            data() {
+                return {
+                    isFormShown: false,
+                    items: [
+                        {
+                            name: "Empty Member"
+                        }
+                    ]
+                };
+            },
+            computed: {
+                ...mapGetters('members', [
+                    'members',
+                    'currentMember'
+                ]),
+                regularComputedProperty () {
+                    return true
                 }
-            ]
-        };
-    },
-    computed: {
-        ...mapGetters('members', [
-            'members',
-            'currentMember'
-        ]),
-        regularComputedProperty () {
-            return true
+            },
+            methods: {
+                ...mapActions('members', [
+                    'addMember'
+                ]),
+                addRandomMember() {
+                    const id = Math.ceil(Math.random() * 100000)
+                    this.addMember({
+                        id,
+                        name: `New Member (${id})`, 
+                        email: 'p@gmail.com',
+                        phone: '5142223344',
+                        studentNumber: '123456789',
+                        programNumber: '123456789',
+                        institution: '123456789',
+                        supervisorName: '123456789',
+                        startDate:'20230518',
+                        endDate:'20230528'  
+                    })
+                }
+            }
         }
-    },
-    methods: {
-        ...mapActions('members', [
-            'addMember'
-        ]),
-        addRandomMember() {
-            const id = Math.ceil(Math.random() * 100000)
-            this.addMember({
-                id,
-                name: `New Member (${id})`,
-                favoriteColor: 'Green'
-            })
-        }
-    }
-}
-</script>
-
-<style scoped>
-</style>
+        </script>
+        
+        <style scoped>
+        </style> 
