b746a21bad89e0bebf551ca74165f7ec38d15b01
houzhihuil <philipwhole@gmail.com>
2023-05-19 17:43:34 -0400

    Create _id.vue

---
+++ pages/members/_id.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        {{ givenMemberId }} - {{ givenMember.name }}
+        <br />
+        <router-link to="../members">Back to list</router-link>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: "ExampleForm",
+    data() {
+        return {};
+    },
+    computed: {
+        ...mapGetters('members',[
+            'members'
+        ]),
+        givenMemberId() {
+            return this.$route.params?.id;
+        },
+        givenMember () {
+            return this.members[this.givenMemberId] || {}
+        }
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+</style>
