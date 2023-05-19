509d22a38a3f01634f2bdcf115d890ed1c7952d6
houzhihuil <philipwhole@gmail.com>
2023-05-19 15:02:23 -0400

    Update MemberForm.vue

--- pages/MemberForm.vue
+++ pages/MemberForm.vue
@@ -1,6 +1,5 @@
 <template>
-    <v-container>
-      <AddButton @click="addRandomMember"></AddButton>
+    <v-container> 
       <AddDialog @member-saved="handleMemberSaved"></AddDialog> 
       <MemberSection2 v-for="member in members" :key="member.id" v-model="member.data"></MemberSection2>
     
@@ -26,32 +25,15 @@
 import { mapGetters, mapActions } from 'vuex' 
 import AddDialog from '~/components/AddDialog.vue';
 import MemberSection2 from '~/components/MemberSection2.vue';
-import AddButton from '~/components/buttons/AddButton.vue';
+ 
         
 export default {
     name: "MemberForm",
-    components: { AddButton, AddDialog, MemberSection2},
+    components: {   AddDialog, MemberSection2},
     layout: "newlayout2",
     data() {
         return {
-            formShown: false,
-              members: 
-            [{
-                id: 1,
-                data: {
-                    id: 1,
-                    name: 'Philip', 
-                    email: 'philip@laresolution.ca',
-                    phone: '4382223678',
-                    studentNumber: '6189853',
-                    programNumber: '9709',
-                    institution: 'G.Godin',
-                    supervisorName: 'Alexandre Vinet',
-                    startDate:'2023-05-19',
-                    endDate:'2023-05-26',
-                }
-            }, 
-            ]  
+            formShown: false, 
         }
     },
     computed: {
@@ -84,22 +66,7 @@ export default {
                 endDate:member.endDate,
         })  
       },
-      addRandomMember() {
-                    const id = Math.ceil(Math.random() * 100000)
-                     
-                    this.addMember({
-                        id,
-                        name: `name(${id})`, 
-                        email: 'philip@gmail.com',
-                        phone: `phonet${id}`,
-                        studentNumber: `student${id}`,
-                        programNumber: `program${id}`,
-                        institution: `student${id}`,
-                        supervisorName: `institution${id}`,
-                        startDate: `startDate${id}`,
-                        endDate: `endDate${id}`,
-                    })
-                }
+ 
     }
 }
 </script>
