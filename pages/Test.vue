0c9d1288be19c0d350afb2f61cd708749e877f35
houzhihuil <philipwhole@gmail.com>
2023-05-19 09:21:40 -0400

    Create Test.vue

---
+++ pages/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <MemberSection2></MemberSection2>
+  </template>
+  
+  <script>
+  import MemberSection2 from '~/components/MemberSection2.vue';
+  
+  export default {
+    name: "Test",
+    components: { MemberSection2 },
+    data() {
+      return {
+        Member: null // Initialize currentMember 
+      };
+    },
+    methods: {
+       
+    }
+  }
+  </script>
+  
