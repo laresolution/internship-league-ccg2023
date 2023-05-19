1284b3e8c488c2cf7a3c742122315f4db2dac764
houzhihuil <philipwhole@gmail.com>
2023-05-18 20:08:20 -0400

    Update TestAddMember3.vue

--- pages/TestAddMember3.vue
+++ pages/TestAddMember3.vue
@@ -1,36 +1,27 @@
 <template>
-    
-    <AddDialog ></AddDialog>
-     
-</template>
-
-<script>
-import AddDialog from '../components/AddDialog.vue'
- 
- 
-export default {
+    <AddDialog @member-saved="handleMemberSaved"></AddDialog>
+  </template>
+  
+  <script>
+  import AddDialog from '../components/AddDialog.vue'
+  
+  export default {
     name: "TestAddMember3",
     components: { AddDialog },
     data() {
-        return {  
-            // members: [], // Array to store new members
- 
-        };
+      return {
+        currentMember: null // Initialize currentMember 
+      };
     },
-    methods: { 
-/*         enregistrerMembre() { //alert('enregistreMembre')
-              // Get the member information from the child component
-            const currentMember = this.$refs.addMember.saveMember();
- 
-              // Save the member information or perform any other desired action
-            console.log(currentMember); // Example: Log the member information to the console
-               
-        }, */
-        test(){
-            alert('hello tiamo')
-           
-        }
-    } 
-}
-
-</script>
+    methods: {
+      handleMemberSaved(member) {
+        console.log(member);
+        // Access the saved member information here
+        this.currentMember = member;
+        console.log('Member info from dialog:'); 
+        console.log(this.currentMember);
+      }
+    }
+  }
+  </script>
+  
