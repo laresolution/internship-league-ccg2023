54ce878d5b47b072e285db3e2030035ebec2b2b2
houzhihuil <philipwhole@gmail.com>
2023-05-18 21:35:33 -0400

    Update MemberForm.vue

--- pages/MemberForm.vue
+++ pages/MemberForm.vue
@@ -1,10 +1,6 @@
 <template>
-    <div>
-        <router-link to="MemberForm">Member Form</router-link> 
-        {{ currentMember.id }}  
-        <AddButton @click="addRandomMember"></AddButton>
-    
-        <AddDialog></AddDialog>
+    <div> 
+        <AddDialog @member-saved="handleMemberSaved"></AddDialog> 
         
         <ul>
             <li v-for="member in members" :key="member.id">
@@ -24,21 +20,15 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import AddButton from '~/components/buttons/AddButton.vue';
  
 import AddDialog from '~/components/AddDialog.vue';
 export default {
     name: "MemberForm",
-    components: { AddButton,  AddDialog},
+    components: { AddDialog},
     layout: "newlayout2",
     data() {
         return {
-            isFormShown: false,
-            items: [
-                {
-                    name: "Empty Member"
-                }
-            ]
+            isFormShown: false, 
         };
     },
     computed: {
@@ -54,21 +44,23 @@ export default {
         ...mapActions('members', [
             'addMember'
         ]),
-        addRandomMember() {
-            const id = Math.ceil(Math.random() * 100000)
-            this.addMember({
-                id,
-                name: `New Member (${id})`, 
-                email: 'p@gmail.com',
-                phone: '5142223344',
-                studentNumber: '123456789',
-                programNumber: '123456789',
-                institution: '123456789',
-                supervisorName: '123456789',
-                startDate:'20230518',
-                endDate:'20230528'  
-            })
-        }
+ 
+        handleMemberSaved(member) { 
+        // Access the saved member information here
+        const id = Math.ceil(Math.random() * 100000)
+        this.addMember({
+            id,
+                name: member.name, 
+                email: member.email,
+                phone: member.phone,
+                studentNumber: member.studentNumber,
+                programNumber: member.programNumber,
+                institution: member.institution,
+                supervisorName: member.supervisorName,
+                startDate:member.startDate,
+                endDate:member.endDate,
+        })  
+      }
     }
 }
 </script>
