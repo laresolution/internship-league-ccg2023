5c93a35f0cd136ddcf7bb4a195dc5f3160d22c80
houzhihuil <philipwhole@gmail.com>
2023-05-19 09:58:07 -0400

    Update Test.vue

--- pages/Test.vue
+++ pages/Test.vue
@@ -1,6 +1,8 @@
 <template>
-    <MemberSection2></MemberSection2>
-  </template>
+    <v-container>
+      <MemberSection2 v-for="member in members" :key="member.id" v-model="member.data"></MemberSection2>
+  </v-container>
+</template>
   
   <script>
   import MemberSection2 from '~/components/MemberSection2.vue';
@@ -9,9 +11,55 @@
     name: "Test",
     components: { MemberSection2 },
     data() {
-      return {
-        Member: null // Initialize currentMember 
-      };
+        return {
+            formShown: false,
+            members: 
+            [{
+                id: 1,
+                data: {
+                    id: 1,
+                    name: 'Philip', 
+                    email: 'philip@laresolution.ca',
+                    phone: '4382223678',
+                    studentNumber: '6189853',
+                    programNumber: '9709',
+                    institution: 'G.Godin',
+                    supervisorName: 'Alexandre Vinet',
+                    startDate:'2023-05-19',
+                    endDate:'2023-05-26',
+                }
+            },
+            { 
+                id: 2,
+                data: {
+                    id: 2,
+                    name: 'Philip', 
+                    email: 'philip@laresolution.ca',
+                    phone: '4382223678',
+                    studentNumber: '6189853',
+                    programNumber: '9709',
+                    institution: 'G.Godin',
+                    supervisorName: 'Alexandre Vinet',
+                    startDate:'2023-05-19',
+                    endDate:'2023-05-26',
+                }
+            },
+            { 
+                id: 3,
+                data: {
+                    id: 3,
+                    name: 'Philip', 
+                    email: 'philip@laresolution.ca',
+                    phone: '4382223678',
+                    studentNumber: '6189853',
+                    programNumber: '9709',
+                    institution: 'G.Godin',
+                    supervisorName: 'Alexandre Vinet',
+                    startDate:'2023-05-19',
+                    endDate:'2023-05-26',
+                }
+            }]
+        }
     },
     methods: {
        
