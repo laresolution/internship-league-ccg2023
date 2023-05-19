19720622a61d1d8a9410da338ccd761879369a8a
houzhihuil <philipwhole@gmail.com>
2023-05-18 22:16:25 -0400

    Create MemberSection2.vue

---
+++ components/MemberSection2.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-spacer></v-spacer>
+            <v-col>
+                <v-img :src="member.imgSrc" max-height="240" max-width="240" />
+            </v-col>
+            <v-col v-show="!isEditing">
+                <v-text-field v-model="valueModel.name" label="Name"></v-text-field>
+  
+                <div class="text-body-1">{{member.name}} </div>
+                <div class="text-body-1">{{member.email}} </div>
+                <div class="text-body-1">{{member.phone}}  </div>   
+                <div class="text-body-1">{{member.studentNumber}} </div>
+                <div class="text-body-1">{{member.programNumber}} </div>
+                <div class="text-body-1">{{member.institution}} </div>
+                <div class="text-body-1">{{member.supervisorName}} </div>
+                <div class="text-body-1">{{member.startDate}} </div>
+                <div class="text-body-1">{{member.endDate}} </div>
+                <v-btn
+                    class="mx-2"
+                    fab
+                    color="primary"
+                    @click="startEdit"
+                >
+                    <v-icon dark>
+                        mdi-pencil
+                    </v-icon>
+                </v-btn>
+            </v-col>
+            <v-col v-show="isEditing">
+                <v-text-field v-model="valueModel.name" label="Name"></v-text-field>
+                <div class="text-body-1">{{member.name}} </div>
+                <div class="text-body-1">{{member.email}} </div>
+                <div class="text-body-1">{{member.phone}}  </div>   
+                <div class="text-body-1">{{member.studentNumber}} </div>
+                <div class="text-body-1">{{member.programNumber}} </div>
+                <div class="text-body-1">{{member.institution}} </div>
+                <div class="text-body-1">{{member.supervisorName}} </div>
+                <div class="text-body-1">{{member.startDate}} </div>
+                <div class="text-body-1">{{member.endDate}} </div>
+                <v-btn
+                    class="mx-2"
+                    fab
+                    color="success"
+                    @click="save"
+                >
+                    <v-icon dark>
+                        mdi-content-save
+                    </v-icon>
+                </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import vModelObjectMixin from '~/mixins/vModelObjectMixin';
+
+export default {
+    name: 'MemberSection',
+    mixins: [vModelObjectMixin],
+    data() {
+        return {
+            isEditing: false
+        }
+    },
+    computed: {
+        member () {
+            return this.value || {};
+        }
+    },
+    methods: {
+        startEdit() {
+            this.isEditing = true;
+        },
+        save () {
+            this.isEditing = false;
+            this.emitValue()
+        },
+        cancel () {
+            this.isEditing = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
