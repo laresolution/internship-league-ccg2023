cd90c87165f5e2c37616e69aeb668c051866eeb3
houzhihuil <philipwhole@gmail.com>
2023-05-19 22:37:01 -0400

    Update MembersCopy.vue

--- pages/MembersCopy.vue
+++ pages/MembersCopy.vue
@@ -7,18 +7,18 @@
                 <AddMember @created="memberCreated" @cancel="hideForm"></AddMember>
             </v-card-text>
         </v-card>
-        <MemberSection v-for="member in members" :key="member.id" v-model="member.data"></MemberSection>
+        <MemberSection2 v-for="member in members" :key="member.id" v-model="member.data"></MemberSection2>
     </div>
 </template>
 
 <script>
 import AddMember from '~/components/AddMember.vue';
-import MemberSection from '~/components/MemberSection.vue';
+import MemberSection2 from '~/components/MemberSection2.vue';
 import AddButton from '~/components/buttons/AddButton.vue';
 
 export default {
     name: "Members",
-    components: { MemberSection, AddButton, AddMember },
+    components: { MemberSection2, AddButton, AddMember },
     data() {
         return {
             formShown: false,
@@ -27,38 +27,17 @@ export default {
                 id: 1,
                 data: {
                     id: 1,
-                    imgSrc: 'https://laresolution.ca/internshipleague/wp-content/uploads/2023/04/vitaly.jpg',
-                    name: 'Vitaly Savin',
-                    title: 'Web Developer',
-                    quote: 'Recently graduated in Web Development and with a solid theoretical base, I am able to learn quickly from Senior Developers and by myself. Strive to solve more complex problems in the most efficient way possible, both as a team and individually.',
-                    strengths: 'I work efficiently in a team and have great autonomy in my learning. Creative and with a good ability to synthetize, I can accurately and quickly accomplish tasks to enhance your business operations',
-                    likes: 'I enjoy running, skiing and learning new things.'
+                    name: 'Philip',
+                    email: 'philip@gmail.com',
+                    phone: '4381234567',
+                    studentNumber: '6189853', 
+                    programNumber: '9706',
+                    institution: 'G.godin',
+                    supervisorName: 'Alex Vinet',
+                    startDate: '2023-5-19',
+                    endDate: '2023-5-29',
                 }
-            },
-            { 
-                id: 2,
-                data: {
-                    id: 2,
-                    imgSrc: 'https://laresolution.ca/internshipleague/wp-content/uploads/2023/04/vitaly.jpg',
-                    name: 'Vitaly Savin Jr',
-                    title: 'Web Developer',
-                    quote: 'Recently graduated in Web Development and with a solid theoretical base, I am able to learn quickly from Senior Developers and by myself. Strive to solve more complex problems in the most efficient way possible, both as a team and individually.',
-                    strengths: 'I work efficiently in a team and have great autonomy in my learning. Creative and with a good ability to synthetize, I can accurately and quickly accomplish tasks to enhance your business operations',
-                    likes: 'I enjoy running, skiing and learning new things.'
-                }
-            },
-            { 
-                id: 3,
-                data: {
-                    id: 3,
-                    imgSrc: 'https://laresolution.ca/internshipleague/wp-content/uploads/2023/04/vitaly.jpg',
-                    name: 'Vitaly Savin Sr',
-                    title: 'Web Developer',
-                    quote: 'Recently graduated in Web Development and with a solid theoretical base, I am able to learn quickly from Senior Developers and by myself. Strive to solve more complex problems in the most efficient way possible, both as a team and individually.',
-                    strengths: 'I work efficiently in a team and have great autonomy in my learning. Creative and with a good ability to synthetize, I can accurately and quickly accomplish tasks to enhance your business operations',
-                    likes: 'I enjoy running, skiing and learning new things.'
-                }
-            }]
+            } ]
         }
     },
     methods: {
