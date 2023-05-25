080e65679c405f38e3ed14f9ec526167745b3d66
vitaly-savin <105252622+vitaly-savin@users.noreply.github.com>
2023-05-25 16:04:48 -0400

    Added to model Availability

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
-    <v-divider :thickness="13" class="border-opacity-100" color="blue"></v-divider>
+    <v-divider
+      :thickness="13"
+      class="border-opacity-100"
+      color="blue"
+    ></v-divider>
     <v-form>
       <form>
-        <v-text-field v-model="newCallback.name" label="First and last name" required>
+        <v-text-field
+          v-model="newCallback.name"
+          label="First and last name"
+          required
+        >
         </v-text-field>
-        <v-text-field v-model="newCallback.email" label="E-mail" required></v-text-field>
-        <v-text-field v-model="newCallback.phoneNumber" label="Phone Number" required></v-text-field>
-        <v-select v-model="select" :items="items" label="Availability" required>
+        <v-text-field
+          v-model="newCallback.email"
+          label="E-mail"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="newCallback.phoneNumber"
+          label="Phone Number"
+          required
+        ></v-text-field>
+        <v-select
+          v-model="newCallback.availability"
+          :items="items"
+          label="Availability"
+          required
+        >
           <v-option v-for="item in items" :key="item">{{ item }}</v-option>
         </v-select>
-        <v-checkbox v-model="checkbox" label="Are you ready?" required></v-checkbox>
-        <v-btn class="mr-4 success" @click="submit">
-          submit
-        </v-btn>
-        <v-btn @click="clear">
-          clear
-        </v-btn>
+        <v-checkbox
+          v-model="checkbox"
+          label="Are you ready?"
+          required
+        ></v-checkbox>
+        <v-btn class="mr-4 success" @click="submit"> submit </v-btn>
+        <v-btn @click="clear"> clear </v-btn>
       </form>
     </v-form>
   </div>
@@ -28,7 +49,7 @@ const generateNewCallback = () => {
     name: '',
     email: '',
     phoneNumber: '',
-    availability: ''
+    availability: '',
   }
 }
 export default {
@@ -39,20 +60,16 @@ export default {
       items: [
         'Morning between 10:00am to 12:00pm',
         'Afternoon between 13:00pm to 15:00pm',
-        'Both'
+        'Both',
       ],
       checkbox: false,
-      newCallback: generateNewCallback()
-
-    };
-
-
+      newCallback: generateNewCallback(),
+    }
   },
   methods: {
     submit() {
-
-      this.$emit("created", this.newCallback);
-      this.newCallback = generateNewCallback();
+      this.$emit('created', this.newCallback)
+      this.newCallback = generateNewCallback()
     },
     clear() {
       this.name = ''
@@ -60,7 +77,7 @@ export default {
       this.phoneNumber = ''
       this.select = null
       this.checkbox = false
-    }
+    },
   },
 }
 </script>
