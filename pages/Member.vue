577c990ed4a471358138695efd1301220584ab05
Zohreh <51000344+PanteaMartia@users.noreply.github.com>
2023-05-26 11:57:45 -0400

    deleting extra

--- pages/Member.vue
+++ pages/Member.vue
@@ -21,44 +21,7 @@ export default {
     components: { MemberSection2, AddButton, AddMember },
     data() {
         return {
-            formShown: false,
-            members: 
-            [{
-                id: 1,
-                data: {
-                    id: 1,
-                    imgSrc: 'https://laresolution.ca/internshipleague/wp-content/uploads/2023/04/vitaly.jpg',
-                    name: 'Vitaly Savin',
-                    title: 'Web Developer',
-                    quote: 'Recently graduated in Web Development and with a solid theoretical base, I am able to learn quickly from Senior Developers and by myself. Strive to solve more complex problems in the most efficient way possible, both as a team and individually.',
-                    strengths: 'I work efficiently in a team and have great autonomy in my learning. Creative and with a good ability to synthetize, I can accurately and quickly accomplish tasks to enhance your business operations',
-                    likes: 'I enjoy running, skiing and learning new things.'
-                }
-            },
-            { 
-                id: 2,
-                data: {
-                    id: 2,
-                    imgSrc: 'https://laresolution.ca/internshipleague/wp-content/uploads/2023/04/vitaly.jpg',
-                    name: 'Vitaly Savin Jr',
-                    title: 'Web Developer',
-                    quote: 'Recently graduated in Web Development and with a solid theoretical base, I am able to learn quickly from Senior Developers and by myself. Strive to solve more complex problems in the most efficient way possible, both as a team and individually.',
-                    strengths: 'I work efficiently in a team and have great autonomy in my learning. Creative and with a good ability to synthetize, I can accurately and quickly accomplish tasks to enhance your business operations',
-                    likes: 'I enjoy running, skiing and learning new things.'
-                }
-            },
-            { 
-                id: 3,
-                data: {
-                    id: 3,
-                    imgSrc: 'https://laresolution.ca/internshipleague/wp-content/uploads/2023/04/vitaly.jpg',
-                    name: 'Vitaly Savin Sr',
-                    title: 'Web Developer',
-                    quote: 'Recently graduated in Web Development and with a solid theoretical base, I am able to learn quickly from Senior Developers and by myself. Strive to solve more complex problems in the most efficient way possible, both as a team and individually.',
-                    strengths: 'I work efficiently in a team and have great autonomy in my learning. Creative and with a good ability to synthetize, I can accurately and quickly accomplish tasks to enhance your business operations',
-                    likes: 'I enjoy running, skiing and learning new things.'
-                }
-            }]
+            formShown: false           
         }
     },
     methods: {
