6215ca094bdb4b0a823f92ed71146050cfbf95fd
houzhihuil <philipwhole@gmail.com>
2023-06-02 10:51:23 -0400

    Create _id.vue

---
+++ pages/products/_id.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container> 
+       
+        <router-link to="../products">Back to list</router-link>
+        <ProductList :value="givenProduct"></ProductList>  
+          
+    </v-container> 
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ProductList from '~/components/ProductList.vue';
+ 
+export default {
+    
+    data() {
+        return {};
+    },
+    components: {ProductList },
+    computed: {
+        ...mapGetters('products',[
+            'products'
+        ]),
+        givenProductId() {
+            return this.$route.params?.id;
+        },
+        givenProduct () {
+            return this.products[this.givenProductId] || {}
+        }
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+</style>
